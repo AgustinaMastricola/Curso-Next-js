810e8477e78d80c3b38735a5d6987bda7508251c
Agus Mastricola <juanz@Juans-MacBook-Pro.local>
2024-06-11 10:50:44 -0300

    Configurando usuario git

---
+++ src/services/explore/explore.service.ts
@@ -0,0 +1,14 @@
+import { MessageType } from "@/types/message.types"
+import { PageType } from "@/types/pagination.types"
+import { UserType } from "@/types/user.types"
+import { httpGet } from "../common/http.service"
+import { Hashtag } from "@/types/tending.types"
+import { URLSearchParams } from "url"
+
+class ExploreAPI {
+    getTrendingHashtag = async (page:number, size:number): Promise<PageType<Hashtag>> => 
+        httpGet(`explore/trending`, new URLSearchParams({page: `${page}`, size: `${size}`}))
+}
+
+const exploreAPI = new ExploreAPI();
+export default exploreAPI;
